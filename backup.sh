2484d1595f3da7dc286dd344f391ee904710c986
rob <rob@unx.pl>
2019-02-15 23:20:01 +0100

    add basic_auth option

--- backup.sh
+++ backup.sh
@@ -3,6 +3,8 @@
 prometheus_pushgateway_url=${PUSHGATEWAY_URL:-http://127.0.0.1:9091}
 prometheus_job=${PROMETHEUS_JOB:-mysqldump-backup}
 override_hostname=${OVERRIDE_HOSTNAME}
+auth_user=${BASIC_AUTH_USER}
+auth_password=${BASIC_AUTH_PASSWORD}
 
 function write_log {
 	echo "`date +'%Y%m%d %H%M%S'`: $1"
@@ -11,10 +13,15 @@ function write_log {
 function notify_prometheus {
 	local success="$1"
 	local duration="$2"
+
+	if [ -n "$auth_user" ] && [ -n "$auth_password" ]; then
+		curl_opts="-u ${auth_user}:$auth_password"
+	fi
+
 	if [ -n "$prometheus_pushgateway_url" ] && [ -n "$prometheus_job" ]; then
 		if [ "$success" -eq 1 ]; then
 			write_log "INFO: notify prometheus: backup success; duration: $duration"
-cat <<EOF | curl -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+cat <<EOF | curl $curl_opts -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
 # HELP mysqldump_backup_duration_seconds Duration of mysqldump backup
 # TYPE mysqldump_backup_duration_seconds gauge
 mysqldump_backup_duration_seconds $duration
@@ -27,7 +34,7 @@ mysqldump_backup_last_success 1
 EOF
 		else
 			write_log "INFO: notify prometheus: backup failed"
-cat <<EOF | curl -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+cat <<EOF | curl $curl_opts -s -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
 # HELP mysqldump_backup_last_success Success of mysqldump backup
 # TYPE mysqldump_backup_last_success gauge
 mysqldump_backup_last_success 0
